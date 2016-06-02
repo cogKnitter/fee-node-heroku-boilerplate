168e6c7d0a926183f9a12b1daa6f9fcfd004992e
Mariah Tree <mariah.tree@gmail.com>
2016-06-01 22:15:09 -0500

    initiate repo

--- modules/App.js
+++ modules/App.js
@@ -6,7 +6,7 @@ export default React.createClass({
   render() {
     return (
       <div>
-        <h1>Mariah Tree's portfolio</h1>
+        <h1>Mariah's portfolio</h1>
         <h2>San Antonio, Texas</h2>
         <p>Hi my name is Mariah. This is a portfolio of my Front End Development projects. I have a MS in Psychology and have spent the last 6+ years as a professional research psychologist. I recently left my research efforts for the military so that I can pursue web development and data visualization full time. </p>
           <ul role="nav">
